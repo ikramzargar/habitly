d30ff69ae26c737586c2aabb8ac750af3efc6fd9
ikramzargar <ikramxargar@gmail.com>
2024-08-28 10:47:13 +0530

    reform setup screen

--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -3,13 +3,17 @@ import 'package:uuid/uuid.dart';
 class Habit {
   final String id;
   final String name;
-  final String iconAssetPath;  // Path for custom asset icons (PNG or other image files)
+  final String iconAssetPath; // Path for custom asset icons (PNG or other image files)
   bool isDone;
+  int hours; // New field for timer duration in hours
+  int minutes; // New field for timer duration in minutes
 
   Habit({
     required this.name,
-    required this.iconAssetPath,  // Always require a custom image path
+    required this.iconAssetPath, // Always require a custom image path
     this.isDone = false,
+    this.hours = 0, // Initialize hours to 0 by default
+    this.minutes = 0, // Initialize minutes to 0 by default
   }) : id = const Uuid().v4();
 
   // Convert the Habit to a map for saving to a database or local storage
@@ -18,7 +22,9 @@ class Habit {
       'id': id,
       'name': name,
       'isDone': isDone,
-      'iconAssetPath': iconAssetPath,  // Save the asset path for the custom image icon
+      'iconAssetPath': iconAssetPath, // Save the asset path for the custom image icon
+      'hours': hours, // Save hours for timer
+      'minutes': minutes, // Save minutes for timer
     };
   }
 
@@ -27,7 +33,9 @@ class Habit {
     return Habit(
       name: map['name'] ?? '',
       isDone: map['isDone'] ?? false,
-      iconAssetPath: map['iconAssetPath'] ?? '',  // Ensure there's always an asset path
+      iconAssetPath: map['iconAssetPath'] ?? '', // Ensure there's always an asset path
+      hours: map['hours'] ?? 0, // Load hours from the map, defaulting to 0
+      minutes: map['minutes'] ?? 0, // Load minutes from the map, defaulting to 0
     );
   }
 }
--- lib/screens/add_habit.dart
+++ lib/screens/add_habit.dart
@@ -1,116 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:habitly/globals/text_styles.dart';
-// import 'package:habitly/models/habit.dart';
-//
-// import '../globals/colors.dart';
-//
-// class AddHabitScreen extends StatefulWidget {
-//   final void Function(Habit) onAddHabit;
-//
-//   const AddHabitScreen({super.key, required this.onAddHabit});
-//
-//   @override
-//   _AddHabitScreenState createState() => _AddHabitScreenState();
-// }
-//
-// class _AddHabitScreenState extends State<AddHabitScreen> {
-//   final TextEditingController _nameController = TextEditingController();
-//   String? _selectedIconPath;
-//
-//   // Predefined icons to choose from (PNG paths)
-//   final List<String> _availableIcons = [
-//     'assets/icons/drink-water.png',
-//     'assets/icons/house.png',
-//     'assets/icons/meditation.png',
-//     'assets/icons/treadmill.png',
-//     // Add more icons here
-//   ];
-//
-//   void _addHabit() {
-//     if (_nameController.text.isNotEmpty && _selectedIconPath != null) {
-//       final newHabit = Habit(
-//         name: _nameController.text,
-//         iconAssetPath: _selectedIconPath!,
-//       );
-//       widget.onAddHabit(newHabit);
-//       Navigator.of(context).pop();
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           'Create custom habit',
-//           style: AppTextStyles.heading2,
-//         ),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(20.0),
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Text(
-//               'What do you want to do?',
-//               style: AppTextStyles.bodyText.copyWith(color: Colors.black),
-//             ),
-//             SizedBox(height: 20.0),
-//             TextFormField(
-//               controller: _nameController,
-//               decoration: InputDecoration(
-//                 hintText: 'Name of habit',
-//                 border: OutlineInputBorder(
-//                   borderSide: const BorderSide(
-//                     color: AppColors.blue,
-//                   ),
-//                   borderRadius: BorderRadius.circular(12),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             Text('Pick an Icon:', style: TextStyle(fontSize: 16)),
-//             SizedBox(height: 10),
-//             DropdownButton<String>(
-//               value: _selectedIconPath,
-//               hint: Text('Select Icon'),
-//               icon: Icon(Icons.arrow_downward),
-//               onChanged: (String? newValue) {
-//                 setState(() {
-//                   _selectedIconPath = newValue;
-//                 });
-//               },
-//               items: _availableIcons
-//                   .map<DropdownMenuItem<String>>((String iconPath) {
-//                 return DropdownMenuItem<String>(
-//                   value: iconPath,
-//                   child: Row(
-//                     children: [
-//                       Image.asset(iconPath, width: 24, height: 24),
-//                       SizedBox(width: 10),
-//                     ],
-//                   ),
-//                 );
-//               }).toList(),
-//             ),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: _addHabit,
-//               child: Text('Add Habit'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-//
-
 import 'package:flutter/material.dart';
 import 'package:habitly/globals/text_styles.dart';
 import 'package:habitly/models/habit.dart';
+import 'package:numberpicker/numberpicker.dart';
 import '../globals/colors.dart';
 
 class AddHabitScreen extends StatefulWidget {
@@ -125,6 +16,10 @@ class AddHabitScreen extends StatefulWidget {
 class _AddHabitScreenState extends State<AddHabitScreen> {
   final TextEditingController _nameController = TextEditingController();
   String? _selectedIconPath;
+  TimeOfDay? _selectedTime;
+  bool _isSwitchOn = false;
+  int _selectedHours = 0;
+  int _selectedMinutes = 0;
 
   // Predefined icons to choose from (PNG paths)
   final List<String> _availableIcons = [
@@ -227,53 +122,103 @@ class _AddHabitScreenState extends State<AddHabitScreen> {
               ),
             ),
             SizedBox(height: 20),
-            GestureDetector(
-              onTap: _showIconPickerDialog,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 35.0),
-                child: Container(
-                  width: 300,
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: AppColors.tile,
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1.0,
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                      decoration: BoxDecoration(
+                        color: AppColors.tile,
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Timer',
+                                style: AppTextStyles.bodyText
+                                    .copyWith(color: Colors.black),
+                              ),
+                              SizedBox(width: 20),
+                            ],
+                          ),
+                          Switch(
+                            activeColor: AppColors.blue,
+                            value: _isSwitchOn,
+                            onChanged: (bool value) {
+                              setState(() {
+                                _isSwitchOn = value;
+                                if (_isSwitchOn) {
+                                 // Show time picker when switch is turned on
+                                }
+                              });
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Pick an icon',
-                        style: AppTextStyles.bodyText
-                            .copyWith(color: Colors.black),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      _selectedIconPath != null
-                          ? Image.asset(
-                              _selectedIconPath!,
-                              width: 24,
-                              height: 24,
-                            )
-                          : Image.asset(
-                              'assets/icons/heart.png',
-                              width: 24,
-                              height: 24,
-                            ),
-                    ],
                   ),
                 ),
-              ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                GestureDetector(
+                  onTap: _showIconPickerDialog,
+                  child: Container(
+                    // width: 300,
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 13),
+                    decoration: BoxDecoration(
+                      color: AppColors.tile,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      // mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Pick an icon',
+                          style: AppTextStyles.bodyText
+                              .copyWith(color: Colors.black),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        _selectedIconPath != null
+                            ? Image.asset(
+                                _selectedIconPath!,
+                                width: 24,
+                                height: 24,
+                              )
+                            : Image.asset(
+                                'assets/icons/heart.png',
+                                width: 24,
+                                height: 24,
+                              ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 20),
-            Text('Settings',style: AppTextStyles.bodyText,),
-
+            Text(
+              'Settings',
+              style: AppTextStyles.bodyText,
+            ),
             ElevatedButton(
               onPressed: _addHabit,
               child: Text('Add Habit'),
@@ -284,3 +229,5 @@ class _AddHabitScreenState extends State<AddHabitScreen> {
     );
   }
 }
+
+
